e4b672a0c7388a63b066762e97c0d6e2cadd52df
Eduardo Martins <eduardomartins.ti@hotmail.com>
2018-07-26 09:59:54 -0300

    Iniciando implementação do Crawler
    - Inclusão da dependencia do Jsoup no pom.xml
    - Implementação inicial da classe responsável por rastrear o conteudo a partir da URL especificada.

---
+++ src/main/java/br/com/editoraglobo/crawler/Crawler.java
@@ -0,0 +1,15 @@
+package br.com.editoraglobo.crawler;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Classe responsável por prover método de rastreamento(crawler) de conteudo a partir de uma URL
+ * @author Eduardo Martins
+ *
+ */
+public class Crawler {
+	
+	private static final Logger LOGGER = LoggerFactory.getLogger(Crawler.class);
+
+}
