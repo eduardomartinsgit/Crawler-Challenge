8c1b7b23b6124c0164f972f5d74a620dbda8eb76
Eduardo Martins <eduardomartins.ti@hotmail.com>
2018-07-26 09:44:57 -0300

    Camada de Segurança
    - Criação da camada básica de segurança para acesso as URIs providas pela aplicação.
    - USER: admin , Password: pass

--- src/main/java/br/com/editoraglobo/configuration/CrawlerChallengeConfiguration.java
+++ src/main/java/br/com/editoraglobo/configuration/CrawlerChallengeConfiguration.java
@@ -1,7 +1,16 @@
 package br.com.editoraglobo.configuration;
 
+import java.util.Collections;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 /**
  * Classe responsável pela inicialização do Spring Boot e suas configurações através de annotações.
@@ -12,5 +21,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class CrawlerChallengeConfiguration {
 	public static void main(String[] args) {
 		SpringApplication.run(CrawlerChallengeConfiguration.class, args);
+	}
+}
+
+@Configuration
+@EnableWebSecurity
+class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+	@Bean
+	UserDetailsService users() {
+		return new InMemoryUserDetailsManager(Collections.singleton(User.withUsername("admin").roles("ADMIN").password("{noop}pass").build()));
 	}
 }
