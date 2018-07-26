dd009ab658a994fe92cf8119cf25d13aefb1847c
Eduardo Martins <eduardomartins.ti@hotmail.com>
2018-07-26 09:11:12 -0300

    Criação do Repositório
    - Criação do Repositório
    - Criação do Projeto com Spring Boot 2.1.0(SNAPSHOT)
    - Spring Boot Web, Security, Devtools

---
+++ src/main/java/br/com/editoraglobo/CrawlerChallenge/CrawlerChallengeApplication.java
@@ -0,0 +1,12 @@
+package br.com.editoraglobo.CrawlerChallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrawlerChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrawlerChallengeApplication.class, args);
+	}
+}
