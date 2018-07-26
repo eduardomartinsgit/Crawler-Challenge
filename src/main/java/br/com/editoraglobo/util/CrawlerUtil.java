c5539d97f06640fbc2116635a27e694aa5c23529
Eduardo Martins <eduardomartins.ti@hotmail.com>
2018-07-26 14:15:22 -0300

    Finalização da Camada de WebService
    - Finalização da camada de webservice.
    - Refactoring

--- src/main/java/br/com/editoraglobo/util/CrawlerUtil.java
+++ src/main/java/br/com/editoraglobo/util/CrawlerUtil.java
@@ -1,5 +1,10 @@
 package br.com.editoraglobo.util;
 
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
+import com.fasterxml.jackson.databind.SerializationFeature;
+
 /**
  * Classe responsável por conter os métodos utilitarios utilizados em toda aplicação.
  * @author Eduar
@@ -7,6 +12,8 @@ package br.com.editoraglobo.util;
  */
 public class CrawlerUtil {
 
+	public static final String REVISTA_AUTO_ESPORTE_FEED = "https://revistaautoesporte.globo.com/rss/ultimas/feed.xml";
+	
 	/**
 	 * Método responsável por verificar se uma URI está vazia.
 	 * @param URI
@@ -18,4 +25,17 @@ public class CrawlerUtil {
 		} 
 		return false;
 	}
+	
+	/**
+	 * Método responsável por configurar o retorno do JSON.
+	 * @return
+	 */
+	public static ObjectWriter writerConfiguration() {
+		//Configuração do Jackson para não exibir propriedades nulas e inserir um valor no ROOT do Json
+		final ObjectMapper mapper = new ObjectMapper().configure(SerializationFeature.WRAP_ROOT_VALUE, true).setSerializationInclusion(Include.NON_NULL); 
+		
+		final ObjectWriter writer = mapper.writer().withRootName("feed");
+		return writer;
+	}	
+	
 }
--- src/main/java/br/com/editoraglobo/webservice/CrawlerWebService.java
+++ src/main/java/br/com/editoraglobo/webservice/CrawlerWebService.java
@@ -10,36 +10,29 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.fasterxml.jackson.annotation.JsonInclude.Include;
 import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.ObjectWriter;
-import com.fasterxml.jackson.databind.SerializationFeature;
 
 import br.com.editoraglobo.crawler.Crawler;
 import br.com.editoraglobo.model.vo.ItemVO;
 import br.com.editoraglobo.util.CrawlerUtil;
 
 @RestController 
-@RequestMapping("/webservice")
 public class CrawlerWebService {
 	
 	private static final Logger LOGGER = LoggerFactory.getLogger(CrawlerWebService.class);
 	
 	@GetMapping("/crawler")
 	@ResponseBody
-	public String rastreamento(@RequestParam(value = "uri", required = false) String URI){
+	public String crawlerWebService(@RequestParam(value = "uri", required = false) String URI){
         try {
         	if(CrawlerUtil.emptyURI(URI)) {
-        		URI = "https://revistaautoesporte.globo.com/rss/ultimas/feed.xml";
+        		URI = CrawlerUtil.REVISTA_AUTO_ESPORTE_FEED;
         	}
         	
         	List<ItemVO> feed = Crawler.crawlerFeed(URI);
         	
-        	//Configuração do Jackson para não exibir propriedades nulas e inserir um valor no ROOT do Json
-        	final ObjectMapper mapper = new ObjectMapper().configure(SerializationFeature.WRAP_ROOT_VALUE, true).setSerializationInclusion(Include.NON_NULL); 
-        	
-        	final ObjectWriter writer = mapper.writer().withRootName("feed");
+        	final ObjectWriter writer = CrawlerUtil.writerConfiguration();
         	
 			return writer.writeValueAsString(feed);
 		} catch (JsonProcessingException e) {
@@ -48,4 +41,5 @@ public class CrawlerWebService {
 		}
        
 	}
+
 }
